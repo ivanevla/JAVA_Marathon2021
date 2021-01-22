ca628b48e76d748072df18c7a897e7862b3ab5b9
Vladislav <1019949+ivanevla@users.noreply.github.com>
2021-01-22 11:18:04 +0200

    complete day3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
 
+        while (true) {
+            double numA = scanner.nextDouble();
+            double numB = scanner.nextDouble();
+
+            if (numB == 0) break;
+
+            double result = numA / numB;
+            System.out.println(result);
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,22 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        for (int i = 1; i <= 5; i++) {
+            double numA = scanner.nextDouble();
+            double numB = scanner.nextDouble();
+
+            if (numB == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+
+            double result = numA / numB;
+            System.out.println(result);
+        }
     }
 }
