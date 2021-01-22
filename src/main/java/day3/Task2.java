8530375ddb0e124a63f32b1883747426ce730426
Vladislav <1019949+ivanevla@users.noreply.github.com>
2021-01-22 11:33:12 +0200

    fix day2

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean exit = false;
 
         while (true) {
             double numA = scanner.nextDouble();
