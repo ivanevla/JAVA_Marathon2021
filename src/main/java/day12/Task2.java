9dc0fa14a888ee10a354fa94e37d3e563e2fa663
Vladislav <1019949+ivanevla@users.noreply.github.com>
2021-02-01 10:52:35 +0200

    fix day12 task2

--- src/main/java/day12/Task2.java
+++ src/main/java/day12/Task2.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 public class Task2 {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(80);
+        List<Integer> list = new ArrayList<>(42);
 
-        for (int i = 0; i < 30; i++) list.add(i);
-        for (int i = 300; i < 350; i++) list.add(i);
+        for (int i = 0; i <= 350; i++) {
+            if (i > 30 && i < 300) continue;
+            if (i % 2 == 0) list.add(i);
+        }
 
         list.forEach(System.out::println);
     }
