fe4448830cf65f92cf74d3e0877b1eb554359f54
Vladislav <1019949+ivanevla@users.noreply.github.com>
2021-02-04 13:23:03 +0200

    complete day16 tasks

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,29 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("D16T1_File.txt");
+        printResult(file);
+    }
 
+    public static void printResult(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            String[] numbersStringArray = scanner.nextLine().split(" ");
+            Double sum = Arrays.stream(numbersStringArray)
+                    .map(Double::parseDouble)
+                    .reduce(0.0, Double::sum);
+            double result = sum / numbersStringArray.length;
+            System.out.printf("%s --> %.3f", result, result);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+            e.printStackTrace();
+        }
     }
 }
 
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,91 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+
 public class Task2 {
     public static void main(String[] args) {
+        File file1 = new File("D16T2_File1.txt");
+        File file2 = new File("D16T2_File2.txt");
 
+        createFile(file1);
+        createFile(file2);
+
+        generateFirstFile(file1);
+        generateSecondFile(file2, file1);
+
+        printResult(file2);
     }
+
+    public static void printResult(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            String[] numbersStringArray = scanner.nextLine().split(" ");
+            double sum = Arrays.stream(numbersStringArray)
+                    .map(Double::parseDouble)
+                    .reduce(0.0, Double::sum);
+            System.out.println((int) sum);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+            e.printStackTrace();
+        }
+    }
+
+    private static void generateFirstFile(File file) {
+        try {
+            PrintWriter printWriter = new PrintWriter(file);
+//            Random random = new Random();
+//            for (int i = 0; i < 1000; i++) {
+//                int randomNumber = random.nextInt(100);
+//                printWriter.printf("%d ", randomNumber);
+//            }
+
+            IntStream.generate(() -> ThreadLocalRandom.current().nextInt(100))
+                    .limit(1000)
+                    .forEach(value -> printWriter.printf("%d ", value));
+            printWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+            e.printStackTrace();
+        }
+    }
+
+    private static void generateSecondFile(File file2, File file1) {
+        try {
+            Scanner scanner = new Scanner(file1);
+            PrintWriter printWriter = new PrintWriter(file2);
+
+            String[] numbersStringArray = scanner.nextLine().split(" ");
+            double sum = 0;
+            for (int i = 1; i <= numbersStringArray.length - 1; i++) {
+                sum += Integer.parseInt(numbersStringArray[i]);
+                if (i % 20 == 0) {
+                    double average = sum / 20;
+                    printWriter.printf("%s ", average);
+                    sum = 0;
+                }
+            }
+            printWriter.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+            e.printStackTrace();
+        }
+    }
+
+    private static void createFile(File file) {
+        try {
+            file.delete();
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
 }
